d4066c72d1a2ac60b783e5db84ea1069218f5c78
Rakshit Ramesh <rakshitadmar@gmail.com>
2017-09-22 11:47:12 +0530

    USed relative paths

--- St_OpenOcd.sh
+++ St_OpenOcd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-while true; do /opt/gnuarmeclipse/openocd/0.10.0-201601101000-dev/bin/openocd -f /opt/gnuarmeclipse/openocd/0.10.0-201601101000-dev/scripts/interface/stlink-v2.cfg -f /opt/gnuarmeclipse/openocd/0.10.0-201601101000-dev/scripts/target/stm32l1.cfg ; sleep 1;done
+while true; do ./openocd/0.10.0-201601101000-dev/bin/openocd -f ./openocd/0.10.0-201601101000-dev/scripts/interface/stlink-v2.cfg -f ./openocd/0.10.0-201601101000-dev/scripts/target/stm32l1.cfg ; sleep 1;done
 
